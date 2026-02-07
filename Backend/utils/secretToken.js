4e41e4dfdbf93de627bf06d19d2a7be48ecdb396
deepak <ds094p12@gmail.com>
2026-02-07 13:29:05 +0530

    solved syntax error in secretToken.js

--- Backend/utils/secretToken.js
+++ Backend/utils/secretToken.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 const createSecretToken = (id) => {
-    return jwt.sign({id}, process.env.Token_key , {
+    return jwt.sign({id}, process.env.TOKEN_KEY , {
         expiresIn: 3*24*60*60,
     });
 };
